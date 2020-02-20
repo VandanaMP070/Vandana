88f8c525e891d89acff64f9a9fb3e06340533a20
Vandana <vandana.mp@sasnee.com>
2020-02-20 12:32:40 +0530

    added with multi function

---
+++ test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("Hello World\n");
+}
+
+	void mult(int a,int b)
+{
+	int c;
+	c=a*b;
+	printf("%d the mult is",c);
+}
+		void add(int a,int b)
+{
+	int c;
+	c=a+b;
+	printf("%d the mult is",c);
+}
+	
